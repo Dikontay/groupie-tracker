a4d1d8e2863e549f62d085efb5ee0ab26c58f473
dalimbay <student@ALEM-F3-L05-09.alem.school>
2023-10-11 12:29:26 +0600

    add go routines and channels to home.go

--- handlers/home.go
+++ handlers/home.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -10,17 +12,49 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 		return
 	}
-	Data := []Artist{}
-	err := getElement(artistUrl, &Data)
+
 	if r.Method != http.MethodGet {
 		http.Error(w, "method not allowed", 405)
 		return
 	}
+	
+	artistsCh := make(chan []Artist, 1)
+	defer close(artistsCh)
+	go func(){
+		artists := []Artist{}
+		err := getElement(artistUrl, &artists)
+		if err != nil {
+			fmt.Println(err)
+			artistsCh <- nil
+			return
+		} else {
+			artistsCh <- artists
+		}
+	}()
+	
+	
 
 	ts, err := template.ParseFiles("./templates/index.html")
 	if err != nil {
 		http.Error(w, "internal server error", 500)
 		return
 	}
-	err = ts.Execute(w, Data)
+
+	select {
+	case artists := <-artistsCh:
+		if artists == nil {
+			http.Error(w, "Not found", http.StatusNotFound)
+		} else {
+			err = ts.Execute(w, artists)
+			if err != nil {
+				http.Error(w, "internal server error", 500)
+				return
+			}
+		}
+	case <-time.After(3*time.Second):
+		http.Error(w, "Request timeout", http.StatusRequestTimeout)
+		return
+
+	}
+	
 }
