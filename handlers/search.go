144675caf70361ff35284fdcaafec78fc23ab3c0
Your Name <you@example.com>
2023-10-19 20:30:24 +0600

    start

---
+++ handlers/search.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"strconv"
+)
+
+func contains(dataset Artist, substr string) []Artist {
+	opt := make(map[string]string)
+	for _, val := range dataset {
+		opt[val.Name] = " -> name"
+
+		for _, mems := range val.Members {
+			opt[mems] = " -> member"
+		}
+		opt[strconv.Itoa(val.CreationDate)] = " -> creation date"
+		opt[val.FirstAlbum] = " -> first album"
+
+		for key, element := range val.DatesLocations {
+			opt[key] = " -> concert location"
+			for _, vals := range element {
+				opt[vals] = " -> concert date"
+			}
+		}
+	}
+	return opt
+}
