331b201c2d6444da9086d5e4e7ef4a1d21055d98
dalimbay <student@ALEM-F4-L04-09.alem.school>
2023-11-07 21:12:17 +0600

    changed style

--- handlers/search.go
+++ handlers/search.go
@@ -1,26 +1,25 @@
-package handlers
+ package handlers
 
-import (
-	"strconv"
-)
+// import  "strconv"
 
-func contains(dataset Artist, substr string) []Artist {
-	opt := make(map[string]string)
-	for _, val := range dataset {
-		opt[val.Name] = " -> name"
 
-		for _, mems := range val.Members {
-			opt[mems] = " -> member"
-		}
-		opt[strconv.Itoa(val.CreationDate)] = " -> creation date"
-		opt[val.FirstAlbum] = " -> first album"
+// func contains(dataset Artist, substr string) []Artist {
+// 	opt := make(map[string]string)
+// 	for _, val := range dataset {
+// 		opt[val.Name] = " -> name"
 
-		for key, element := range val.DatesLocations {
-			opt[key] = " -> concert location"
-			for _, vals := range element {
-				opt[vals] = " -> concert date"
-			}
-		}
-	}
-	return opt
-}
+// 		for _, mems := range val.Members {
+// 			opt[mems] = " -> member"
+// 		}
+// 		opt[strconv.Itoa(val.CreationDate)] = " -> creation date"
+// 		opt[val.FirstAlbum] = " -> first album"
+
+// 		for key, element := range val.DatesLocations {
+// 			opt[key] = " -> concert location"
+// 			for _, vals := range element {
+// 				opt[vals] = " -> concert date"
+// 			}
+// 		}
+// 	}
+// 	return opt
+// }
